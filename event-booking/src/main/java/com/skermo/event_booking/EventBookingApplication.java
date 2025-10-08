64fc9821e66987e67b65d77538a22eda06ad8342
skermo <kermosemra@gmail.com>
2025-10-08 20:07:11 +0200

    Backend: Create Backend

---
+++ event-booking/src/main/java/com/skermo/event_booking/EventBookingApplication.java
@@ -0,0 +1,13 @@
+package com.skermo.event_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBookingApplication.class, args);
+	}
+
+}
